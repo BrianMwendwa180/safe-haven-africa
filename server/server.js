f6a9623fe529d9273545e9d80636c76a5f588920
BrianMwendwa180 <brianmwendwa180@gmail.com>
2025-12-14 22:20:05 +0300

    real time chat server deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST"]
   }
 });
